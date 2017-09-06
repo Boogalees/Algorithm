728b225651dcbc5081b44abcf7c21ae19cc70ee5
MooyeolKim <kmyms2@naver.com>
2017-09-07 02:22:59 +0900

    Runtime error

--- Algorithm/Search_1/1963/1963.cpp
+++ Algorithm/Search_1/1963/1963.cpp
@@ -1,99 +1,61 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <algorithm>
-
+#include <queue>
+#include <string>
 using namespace std;
+
 bool check[10001];
 int dist[10001];
+bool prime[10001];
+
+int change(int k, int l, int m) {
+	if (l == 0 && m == 0) return -1;
+	string s = to_string(k);
+	s[l] = m;
+	return stoi(s);
+}
 
 int main(void) {
-	int testCase;
-	int p1, p2;
-	scanf("%d", &testCase);
+	int T, p1, p2;
+	queue<int> q;
 
-	for (int i = 0; i < 10001; i++) {
-		for (int j = 2; j < i; j++) {
-			if (i%j == 0) {
-				check[i] = true;
-				break;
-			}
+	scanf("%d", &T);
+
+	for (int i = 1000; i < 10000; i++) {
+		for (int j = 2; j <= i; j++) {
+			if (i%j == 0)
+				prime[i] = true; //ì†Œìˆ˜ê°€ ì•„ë‹˜
 		}
 	}
 
-	for (int i = 0; i < testCase; i++) {
+	while (T--) {
 		scanf("%d %d", &p1, &p2);
-		
-		queue<int> q;
+
 		q.push(p1);
+		check[p1] = true;
 		dist[p1] = 0;
 		while (!q.empty()) {
 			int now = q.front();
 			q.pop();
 			
-			//1000ÀÚ¸´¼ö º¯°æ
-			for (int i = 1; i < 10; i++) {
-				if (i == now / 1000)
-					continue;
-				else {
-					int tmp = i * 1000 + now % 1000;
-					if (check[tmp] == false) {
-						q.push(tmp);
-						dist[tmp] = dist[now] + 1;
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 10; j++) {
+					int tmp = change(now, i, j);
+					if (tmp != -1) {
+						if (prime[tmp] && check[tmp] == false) {
+							q.push(tmp);
+							check[tmp] = true;
+							dist[tmp] = dist[now] + 1;
+						}
 					}
-
+					
 				}
-
 			}
 
-			//100ÀÚ¸´¼ö º¯°æ
-			for (int i = 1; i < 10; i++) {
-				if (i == (now%1000)/100)
-					continue;
-				else {
-					int tmp = (now - now % 1000) + i * 100 + now % 100;
-					if (check[tmp] == false) {
-						q.push(tmp);
-						dist[tmp] = dist[now] + 1;
-					}
-
-				}
-
-			}
-
-			//10ÀÚ¸´¼ö º¯°æ
-			for (int i = 1; i < 10; i++) {
-				if (i == (now %100)/10)
-					continue;
-				else {
-					int tmp = (now - now % 100) + i * 10 + now % 10;
-					if (check[tmp] == false) {
-						q.push(tmp);
-						dist[tmp] = dist[now] + 1;
-					}
-
-				}
-
-			}
-
-			//1ÀÚ¸´¼ö º¯°æ
-			for (int i = 1; i < 10; i++) {
-				if (i == now%10)
-					continue;
-				else {
-					int tmp = (now - now % 10) + i;
-					if (check[tmp] == false) {
-						q.push(tmp);
-						dist[tmp] = dist[now] + 1;
-					}
-
-				}
-
-			}
 		}
-		printf("%d\n", dist[p2]);
-	}
 
+	
+	}
 
 	return 0;
 }
